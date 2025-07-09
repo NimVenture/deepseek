c6a0f9d2522b5319023e4434da91dea5ff296323
bung87 <crc32@qq.com>
2025-07-09 21:41:14 +0800

    feat: add content type checks for JSON parsing in error handling
    - Add strutils import for startsWith function
    - Add content type validation before JSON parsing in 401 and 429 error cases
    - Wrap JSON parsing in try-catch blocks for robustness
    - Fall back to generic error messages when content type is not JSON
    - Bump version to 0.1.3

--- src/deepseek/common.nim
+++ src/deepseek/common.nim
@@ -1,4 +1,4 @@
-import std/[httpcore,json]
+import std/[httpcore,json,strutils]
 import puppy
 import ./[types, exceptions]
 
@@ -9,13 +9,20 @@ proc getException*(resp: Response): ref exceptions.DeepSeekError =
   of 0:
     result = newException(InvalidAPIKeyError, "")
   of 401:
-    let data = parseJson(resp.body)
-    let err = data{"error"}.to(DeepSeekErrorData)
-    case err.code
-      of "invalid_api_key":
-        result = newException(InvalidAPIKeyError, err.message)
+    try:
+      let contentType = resp.headers["content-type"]
+      if contentType.startsWith("application/json"):
+        let data = parseJson(resp.body)
+        let err = data{"error"}.to(DeepSeekErrorData)
+        case err.code
+          of "invalid_api_key":
+            result = newException(InvalidAPIKeyError, err.message)
+          else:
+            result = newException(AuthenticationError, $Http401)
       else:
         result = newException(AuthenticationError, $Http401)
+    except:
+      result = newException(AuthenticationError, $Http401)
   of 402:
     result = newException(InsufficientQuotaError, $Http402)
   # of 403:
@@ -27,9 +34,16 @@ proc getException*(resp: Response): ref exceptions.DeepSeekError =
   of 422:
     result = newException(UnprocessableEntityError, $Http422)
   of 429:
-    let data = parseJson(resp.body)
-    let err = data{"error"}.to(DeepSeekErrorData)
-    result = newException(RateLimitExceededError, err.message)
+    try:
+      let contentType = resp.headers["content-type"]
+      if contentType.startsWith("application/json"):
+        let data = parseJson(resp.body)
+        let err = data{"error"}.to(DeepSeekErrorData)
+        result = newException(RateLimitExceededError, err.message)
+      else:
+        result = newException(RateLimitExceededError, $Http429)
+    except:
+      result = newException(RateLimitExceededError, $Http429)
   of 500:
     result = newException(InternalServerError, $Http500)
   of 503:
